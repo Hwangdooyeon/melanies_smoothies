a84f7685b64dd9a8cfcb92b0e96874fb330bcb4f
Hwang doo yeon <yueuch102@naver.com>
2025-08-27 13:30:51 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,6 @@ import streamlit as st
 from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 # Write directly to the app
-st.title("과일 선택해보기")
 st.write(
   """wiz-con
   """
